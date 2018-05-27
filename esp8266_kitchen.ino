32ca7d8004db46e0644a0713eea41e9b5b6aa18a
guyco75 <x>
2018-05-27 20:47:17 +0300

    make the main file generic and introduce kitchen-profile.h

--- esp8266_kitchen.ino
+++ esp8266_kitchen.ino
@@ -4,11 +4,12 @@
 #include "credentials.h"
 
 #define BUILTIN_LED (2)
-#define MQTT_CLIENT_ID "Kitchen-ESP8266"
-#define TOPIC_PREFIX "home/f1/kitchen"
 
 WiFiClient espClient;
 PubSubClient client(mqtt_server, 1883, espClient);
+SerialParser serParser(128);
+
+#include "kitchen_profile.h"
 
 void setup_wifi() {
   WiFi.begin(ssid, password);
@@ -20,29 +21,26 @@ void setup_wifi() {
   }
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  char serial_str[40];
-  char str[16];
+void callback(char *topic, byte *payload, unsigned int length) {
+  char str[80];
 
   if (length >= sizeof(str)) {
-    client.publish("dbg", "err: callback - length");
+    client.publish(TOPIC_PREFIX, "err: callback - length");
     return;
   }
   memcpy(str, payload, length);
   str[length] = '\0';
 
-  if (!strcmp(TOPIC_PREFIX "/light/0/set", topic)) {
-    snprintf(serial_str, sizeof(serial_str), "$0,%s,#", str);
-    Serial.print(serial_str);
-    client.publish("dbg" TOPIC_PREFIX, serial_str);
-  }
+  handle_mqtt_message(topic, str, length);
 }
 
 void reconnect() {
+  char alive[40];
   while (!client.connected()) {
     if (client.connect(MQTT_CLIENT_ID)) {
-      client.subscribe(TOPIC_PREFIX "/light/+/set");
-      client.publish(TOPIC_PREFIX "/status", "alive");
+      subscribe();
+      snprintf(alive, sizeof(alive), "alive %d.%d.%d.%d", WiFi.localIP()[0],WiFi.localIP()[1],WiFi.localIP()[2],WiFi.localIP()[3]);
+      client.publish(TOPIC_PREFIX "/status", alive);
     } else {
       //TODO: blink
       // Wait 5 seconds before retrying
@@ -53,30 +51,9 @@ void reconnect() {
 
 void setup() {
   pinMode(BUILTIN_LED, OUTPUT);
-  Serial.begin(9600);
+  Serial.begin(SERIAL_SPEED);
   setup_wifi(); // TODO: Anything to do in reconnect?
   client.setCallback(callback);
-}
-
-SerialParser serParser(128);
-
-void handleSerialCmd() {
-  char topic[40];
-  char payload[16];
-  int cmd, l, br;
-
-  cmd = serParser.getNextToken().toInt();
-  if (cmd == 0) {
-    l = serParser.getNextToken().toInt();
-    if (l < 0 || 15 < l) {client.publish("dbg", "err: serial - light");return;}
-  
-    br = serParser.getNextToken().toInt();
-    if (br < 0 || 100 < br) {client.publish("dbg", "err: serial - brightness");return;}
-
-    snprintf(topic, sizeof(topic), TOPIC_PREFIX "/light/%d/update", l);
-    snprintf(payload, sizeof(payload), "%d", br);
-    client.publish(topic, payload);
-  }
 }
 
 void loop() {
---
+++ kitchen_profile.h
@@ -0,0 +1,38 @@
+#define MQTT_CLIENT_ID "Kitchen-ESP8266"
+#define TOPIC_PREFIX "home/f1/kitchen"
+#define SERIAL_SPEED (57600)
+
+static void subscribe() {
+  client.subscribe(TOPIC_PREFIX "/light/+/set");
+}
+
+static void handle_mqtt_message(char *topic, char *str, unsigned int length) {
+  char serial_str[40];
+
+  if (!strcmp(TOPIC_PREFIX "/light/0/set", topic)) {
+    snprintf(serial_str, sizeof(serial_str), "$0,%s,#", str);
+    Serial.print(serial_str);
+    client.publish("dbg" TOPIC_PREFIX, serial_str);
+  }
+}
+
+static void handleSerialCmd() {
+  char topic[40];
+  char payload[16];
+  int cmd, l, br;
+
+  cmd = serParser.getNextToken().toInt();
+  if (cmd == 0) {
+    l = serParser.getNextToken().toInt();
+    if (l < 0 || 15 < l) {client.publish("dbg", "err: serial - light");return;}
+
+    br = serParser.getNextToken().toInt();
+    if (br < 0 || 100 < br) {client.publish("dbg", "err: serial - brightness");return;}
+
+    snprintf(topic, sizeof(topic), TOPIC_PREFIX "/light/%d/update", l);
+    snprintf(payload, sizeof(payload), "%d", br);
+    client.publish(topic, payload);
+  }
+}
+
+
